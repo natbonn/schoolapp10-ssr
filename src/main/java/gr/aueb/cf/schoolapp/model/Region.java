a885bc9fd599db5009e9bef6e772ab42c5a8f7f3
natbonn <natbon@hotmail.gr>
2026-09-09 11:59:34 +0300

    add relationship between Region and Teacher entities, implement methods to manage teachers in Region

--- src/main/java/gr/aueb/cf/schoolapp/model/Region.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/Region.java
@@ -1,16 +1,21 @@
 package gr.aueb.cf.schoolapp.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "regions")             // δεν χρειάζεται timestamp - εδώ - μπορεί σε άλλο app ναι
-public class Region {                // παραμετρικός πίνακας για combo box - όχι extended
+@Table(name = "regions")                   // δεν χρειάζεται timestamp - εδώ - μπορεί σε άλλο app ναι
+public class Region {                      // παραμετρικός πίνακας για combo box - όχι extended
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +23,23 @@ public class Region {                // παραμετρικός πίνακας 
 
     @Column(unique = true, nullable = false)
     private String name;
+
+    @Getter(AccessLevel.PACKAGE)
+    @Setter(AccessLevel.NONE)                               // δεν θέλουμε να αλλάζει το Set από έξω μόνο να προσθέτουμε ή να αφαιρούμε αντικείμενα με addTeacher και removeTeacher
+    @OneToMany(mappedBy = "region", fetch = FetchType.LAZY)
+    private Set<Teacher> teachers = new HashSet<>();        // όχι διπλότυπα
+
+    public Set<Teacher> getAllTeachers() {
+        return Collections.unmodifiableSet(teachers) ;      // επιστρέφει αντίγραφο του Set για να μην μπορεί να αλλάξει από έξω
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+        teacher.setRegion(this);                          // set the region of the teacher to this region
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        teachers.remove(teacher);
+        teacher.setRegion(null);                          // set the region of the teacher to null
+    }
 }
--- src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @Table(name = "teachers")
-public class Teacher extends AbstractEntity {       // για να κληρονομήσει τα πεδία createdAt και updatedAt
+public class Teacher extends AbstractEntity {              // για να κληρονομήσει τα πεδία createdAt και updatedAt
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,14 +24,18 @@ public class Teacher extends AbstractEntity {       // για να κληρον�
     private UUID uuid;
 
     @Column(unique = true, nullable = false)
-    private String vat;                            // European VAT number
+    private String vat;                                    // European VAT number
 
     private String firstname;
     private String lastname;
 
+    @JoinColumn(name = "region_id")                        // foreign key column name
+    @ManyToOne(fetch = FetchType.LAZY)                     // εδώ το default είναι EAGER, αλλά το αλλάζουμε σε LAZY για να μην επιβαρύνεται το app
+    private Region region;
+
     @PrePersist
     public void initializeUUID() {
-        if (uuid == null) this.uuid = UUID.randomUUID();    // if null new UUID is generated
+        if (uuid == null) this.uuid = UUID.randomUUID();   // if null new UUID is generated
 
     }
 
