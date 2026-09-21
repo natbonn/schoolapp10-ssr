d677b461ea7419fd8ec39d88a3b130882de548de
natbonn <natbon@hotmail.gr>
2026-09-21 17:10:12 +0300

    feat(user): implement user registration functionality with role assignment and success page

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -2,7 +2,9 @@ package gr.aueb.cf.schoolapp.mapper;
 
 import gr.aueb.cf.schoolapp.dto.*;
 import gr.aueb.cf.schoolapp.model.Region;
+import gr.aueb.cf.schoolapp.model.Role;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.model.User;
 import org.springframework.stereotype.Component;
 
 @Component      // για να μπορούμε να την κάνουμε inject όπου χρειάζεται
@@ -25,5 +27,17 @@ public class Mapper {
     public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher) {
         return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(),
                 teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
+    }
+
+    public User mapToUserEntity(UserInsertDTO userInsertDTO) {
+        return new User(userInsertDTO.username(), userInsertDTO.password());
+    }
+
+    public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
+        return new UserReadOnlyDTO(user.getUuid().toString(), user.getUsername(), user.getRole().getName());
+    }
+
+    public RoleReadOnlyDTO mapToRoleReadOnlyDTO(Role role) {
+        return new RoleReadOnlyDTO(role.getId(), role.getName());
     }
 }
