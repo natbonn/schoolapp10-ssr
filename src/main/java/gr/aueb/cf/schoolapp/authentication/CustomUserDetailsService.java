ffcbea11dbf91c68596bba8c35d630b74c749e7a
natbonn <natbon@hotmail.gr>
2026-09-26 14:01:55 +0300

    feat(authentication): implement custom user details service for user login

---
+++ src/main/java/gr/aueb/cf/schoolapp/authentication/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.schoolapp.authentication;
+
+import gr.aueb.cf.schoolapp.repository.UserRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+// Φορτώνει τον χρήστη όταν γίνεται το login
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+}
